32e9df8d6da0635fe56c91282f98f5df3ff69acf
HappyISmile <s7125@mail.ru>
2024-04-06 12:31:28 +0500

    save image

--- src/imgShow.java
+++ src/imgShow.java
@@ -1,7 +1,6 @@
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfByte;
+import org.opencv.core.*;
 import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
 import javax.swing.*;
 
@@ -18,8 +17,22 @@ public class imgShow extends JFrame {
 
         Mat img = Imgcodecs.imread("src/fireOnBox.jpg");
 
+        Mat imgEmpty = new Mat(img.size(), img.type());
+
+        Mat kernel = new Mat(5, 5, CvType.CV_8UC1, new Scalar(1.0));
+
+//        Imgproc.dilate(img, imgEmpty, kernel);
+//        Imgproc.erode(img, imgEmpty, kernel);
+//        Imgproc.cvtColor(img, imgEmpty, Imgproc.COLOR_BGR2GRAY);
+//        Imgproc.GaussianBlur(img, imgEmpty, new Size(15, 15), 0);
+//        Imgproc.Canny(img, imgEmpty, 2, 2);
+
+        Imgcodecs.imwrite("src\\saveImg.png", img);
+
+        Mat imgCrop = img.rowRange(100, 250);
+
         MatOfByte buf = new MatOfByte();
-        Imgcodecs.imencode(".jpg", img, buf);
+        Imgcodecs.imencode(".jpg", imgEmpty, buf);
 
         ImageIcon ic = new ImageIcon(buf.toArray());
 
